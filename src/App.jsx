c71be36bb7a3ad7260a73ca798d6eda84128dc19
Phogli92 <phogligamer92@gmail.com>
2024-07-21 11:34:09 +0500

    This project is finished by:Fox🦊

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  let [userData, setUserData] = useState([])
+  function Refresh (){
+    location.reload()
+  }
+      useEffect(()=>{
+      async function getUser(){
+          let response = await fetch ("https://random-data-api.com/api/v2/users")
+          let data = await response.json()
+        setUserData(data)
+      }
+      getUser()
+  }) 
+
+
+  return <>
+  {console.log(userData)}
+  <img src={userData.avatar} alt="" />
+  <h1>Mr.{userData.first_name} {userData.last_name}</h1>
+  <h3>Birthday🎉: {userData.date_of_birth}</h3>
+  <p>Email📩: {userData.email}</p>
+  <p>Phone📞: {userData.phone_number}</p>
+
+  <button onClick={Refresh}>Refresh🔍</button>
+  </>
+
+}
+export default App
